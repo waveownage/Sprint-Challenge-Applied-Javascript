ea8ec7356d56f0aacbbba97787ae43f243d9dd2b
Andre Freeman <asf@mail.usf.edu>
2020-02-24 20:50:55 -0500

    code will work now; going to try to itterate over object

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,37 +17,56 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
-
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
-  .then(response => {
-    console.log(response)
-  })
-  .catch(err => {})
+.then(response => {
+  const newArrayJs = response.data.articles.javascript;
+  console.log(newArrayJs);
+  newArrayJs.forEach((curr) => {
+    console.log(curr);
+    newJs = cardComponent(curr);
+    cardContainer.appendChild(newJs);
+  });
+  const newArrayBS = response.data.articles.bootstrap;
+  const newArrayTech = response.data.articles.technology;
+  const newArrayJq = response.data.articles.jquery;
+  const newArrayNode = response.data.articles.node;
+
+  console.log(newArrayJs);
+  
+})
+.catch(err => {
+  console.log('ERROR')
+})
+
 
 const cardComponent = (object) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
+  const card = document.createElement('div');
+  card.classList.add('card');
 
-    const headline = document.createElement('div');
-    headline.classList.add('headline');
+  const cardHeadline = document.createElement('div');
+  cardHeadline.classList.add('headline');
+  cardHeadline.textContent = object.headline;
 
-    const author = document.createElement('div');
-    author.classList.add('author');
+  const author = document.createElement('div');
+  author.classList.add('author');
 
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('img-container');
+  const imgContainer = document.createElement('div');
+  imgContainer.classList.add('img-container');
 
-    const img = document.createElement('img');
+  const imgCard = document.createElement('img');
+  imgCard.scr = object.authorPhoto;
 
-    const authorName = document.createElement('span');
+  const nameAuthor = document.createElement('span');
+  nameAuthor.textContent = `By ${object.authorName}`;
 
+  card.appendChild(cardHeadline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(nameAuthor);
+  imgContainer.appendChild(imgCard);
 
-    card.appendChild(headline);
-    card.appendChild(author);
-    author.appendChild(imgContainer);
-    author.appendChild(authorName);
-    imgContainer.appendChild(img);
+  return card;
+  
+};
 
-    return cardComponent
-
-}
+const cardContainer = document.querySelector('.cards-container');
